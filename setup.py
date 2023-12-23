186318e47caae886a69cc8eafe6a74652fcfdc55
sibasispradhan <2021sc04667@wilp.bits-pilani.ac.in>
2023-12-23 21:42:32 +0530

    chatbot setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sibasis Pradhan',
+    author_email= '2021sc04667@wilp.bits-pilani.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
